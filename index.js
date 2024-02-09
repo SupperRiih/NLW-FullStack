7e6276f212e419400563d48854cd346d5b92b60d
Ricardo Mitsuo <156557462+SupperRiih@users.noreply.github.com>
2024-02-08 23:52:38 -0300

    new

--- index.js
+++ index.js
@@ -100,32 +100,32 @@ const perguntas = [
   mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
   
   //loop ou laço de repetição
-  for(const item of perguntas) {
-    const quizItem = template.content.cloneNode(true)
-    quizItem.querySelector('h3').textContent = item.pergunta
-    
-    for(let resposta of item.respostas) {
-      const dt = quizItem.querySelector('dl dt').cloneNode(true)
-      dt.querySelector('span').textContent = resposta
-      dt.querySelector('input').setAttribute('name', 'pergunta-'  + perguntas.indexOf(item))
-      dt.querySelector('input').value = item.respostas.indexOf(resposta)
-      dt.querySelector('input').onchange = (event) => {
-      const estaCorreta = event.target.value == item.correta
-      
-      corretas.delete(item)
-      if(estaCorreta) {
-          corretas.add(item)
-        }
-  
-        mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
-      }
-      
-     
-      quizItem.querySelector('dl').appendChild(dt)
+    for(const item of perguntas) {
+        const quizItem = template.content.cloneNode(true)
+        quizItem.querySelector('h3').textContent = item.pergunta
+
+       for(let resposta of item.respostas) {
+            const dt = quizItem.querySelector('dl dt').cloneNode(true)
+            dt.querySelector('span').textContent = resposta
+            dt.querySelector('input').setAttribute('name', 'pergunta-'  + perguntas.indexOf(item))
+            dt.querySelector('input').value = item.respostas.indexOf(resposta)
+            dt.querySelector('input').onchange = (event) => {
+            const estaCorreta = event.target.value == item.correta
+            
+            corretas.delete(item)
+                if(estaCorreta) {
+                        corretas.add(item)
+                }
+            
+                    mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+                }
+                
+                
+            quizItem.querySelector('dl').appendChild(dt)
+            }
+        
+        quizItem.querySelector('dl dt').remove()
+        
+        //coloca a pergunta na tela
+        quiz.appendChild(quizItem)
     }
-    
-    quizItem.querySelector('dl dt').remove()
-    
-    //coloca a pergunta na tela
-    quiz.appendChild(quizItem)
-  }
